7197691c6485abd0556277da926f0bdebf9b1f3c
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-05-21 10:13:23 -0300

    :sparkles: feat : Configuring the path to locate id

--- src/main/java/com/fatec/student/services/StudentService.java
+++ src/main/java/com/fatec/student/services/StudentService.java
@@ -2,11 +2,14 @@ package com.fatec.student.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fatec.student.entities.Student;
 import com.fatec.student.repositories.StudentRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class StudentService {
@@ -16,6 +19,14 @@ public class StudentService {
     
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public Student getStudentById(int id){
+
+        return studentRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Aluno não cadastrado")
+        );
+
     }
 
 
