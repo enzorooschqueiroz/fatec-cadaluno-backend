d0fa989189c15f22ded78e486f8bd095ccbd08f8
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-05-28 09:07:39 -0300

    :sparkles: feat: update user

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +38,14 @@ public class StudentController {
     this.studentService.deleteStudentById(id);
   }
 
-    @PostMapping
-    public Student createStudent(@RequestBody Student student) {
+  @PostMapping
+  public Student createStudent(@RequestBody Student student) {
     return this.studentService.save(student);
   }
+
+  @PutMapping("{id}")
+  public void update(@PathVariable int id, @RequestBody Student student){
+    this.studentService.update(id, student);
+  }
+
 }
