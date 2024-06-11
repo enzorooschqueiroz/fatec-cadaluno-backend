5b2977a73e35536e12be4b1c1ebee33f3b633b56
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-06-11 10:58:21 -0300

    :sparkles: feat: Cross origin

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.fatec.student.dto.StudentResponse;
 import com.fatec.student.services.StudentService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200" )
 @RequestMapping("students")
 public class StudentController {
 
